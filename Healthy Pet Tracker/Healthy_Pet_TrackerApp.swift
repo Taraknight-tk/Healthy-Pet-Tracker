06cd15f0b56b49be21fd1a09185c4f4664bad712
taraknight-tk <tara.knight@outlook.com>
2026-01-29 13:01:22 -0700

    Initial Commit

---
+++ Healthy Pet Tracker/Healthy_Pet_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healthy_Pet_TrackerApp.swift
+//  Healthy Pet Tracker
+//
+//  Created by Tara Knight on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Healthy_Pet_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
